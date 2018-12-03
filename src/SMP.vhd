9e68049cbf9ceb43971047d9efdd3c80792faefa
Sergey Dvodnenko <dvodnenkos@yandex.ua>
2018-12-03 19:15:35 +0200

    SMP timers and IO ports fixes

--- src/SMP.vhd
+++ src/SMP.vhd
@@ -38,14 +38,18 @@ architecture rtl of SMP is
 
 	signal SPC700_D_IN, SPC700_D_OUT : std_logic_vector(7 downto 0);
 	signal SPC700_A : std_logic_vector(15 downto 0);
-	signal SPC700_WE : std_logic;
-	signal EN : std_logic;
+	signal SPC700_R_WN : std_logic;
+	signal SPC700_CE : std_logic;
+	signal TIMER_CE : std_logic;
 	
 	type Port_t is array (0 to 3) of std_logic_vector(7 downto 0);
 	signal CPUI : Port_t;
 	signal CPUO : Port_t;
 	
-	signal TEST : std_logic_vector(7 downto 0);
+	signal CLK_SPEED : std_logic_vector(1 downto 0);
+	signal TM_SPEED : std_logic_vector(1 downto 0);
+	signal TIMERS_ENABLE, TIMERS_DISABLE : std_logic;
+	signal RAM_WRITE_EN : std_logic;
 	signal TM_EN : std_logic_vector(2 downto 0);
 	signal IPL_EN : std_logic;
 	signal T0DIV : std_logic_vector(7 downto 0);
@@ -54,11 +58,13 @@ architecture rtl of SMP is
 	signal T0OUT : std_logic_vector(3 downto 0);
 	signal T1OUT : std_logic_vector(3 downto 0);
 	signal T2OUT : std_logic_vector(3 downto 0);
+	type Aux_t is array (0 to 1) of std_logic_vector(7 downto 0);
+	signal AUX : Aux_t;
 	
 	signal RESET_PORT : std_logic_vector(1 downto 0);
-	signal TM_CNT : unsigned(6 downto 0) := (others=>'0');
+	signal TM01_CNT : unsigned(8 downto 0);
+	signal TM2_CNT : unsigned(5 downto 0);
 	signal T0_CNT, T1_CNT, T2_CNT : unsigned(7 downto 0);
-	signal RESET01, RESET23 : std_logic;
 	
 	signal PAWR_Nr : std_logic_vector(3 downto 0);
 	
@@ -132,7 +138,7 @@ architecture rtl of SMP is
 
 begin
 
-	EN <= ENABLE and CE;
+	SPC700_CE <= ENABLE and CE;
 	
 	process(RST_N, CLK)
 	begin
@@ -150,7 +156,7 @@ begin
 						when others => null;
 					end case;
 				end if;
-			elsif EN = '1' then
+			elsif SPC700_CE = '1' then
 				if RESET_PORT(0) = '1' then
 					CPUI(0) <= (others=>'0');
 					CPUI(1) <= (others=>'0');
@@ -177,12 +183,12 @@ begin
 	port map (
 		CLK      	=> CLK,
 		RST_N    	=> RST_N,
-		RDY      	=> EN,
+		RDY      	=> SPC700_CE,
 		IRQ_N 		=> '1',
 		A_OUT 		=> SPC700_A,
 		D_IN     	=> SPC700_D_IN,
 		D_OUT    	=> SPC700_D_OUT,
-		WE       	=> SPC700_WE,
+		WE       	=> SPC700_R_WN,
 		
 		DBG_REG     => DBG_REG,
 		DBG_DAT_IN	=> DBG_DAT_IN,
@@ -193,13 +199,17 @@ begin
 	
 	
 	process(CLK, RST_N)
+		variable TM_STEP : unsigned(4 downto 0);
+		variable NEW_TM01_CNT : unsigned(8 downto 0);
+		variable NEW_TM2_CNT : unsigned(5 downto 0);
 	begin
 		if RST_N = '0' then
-			TEST <= x"0A";
+			CLK_SPEED <= (others=>'0');
+			TM_SPEED <= (others=>'0');
+			TIMERS_ENABLE <= '1';
+			TIMERS_DISABLE <= '0';
+			RAM_WRITE_EN <= '1';
 			IPL_EN <= '1';
--- synthesis translate_off
---			IPL_EN <= '0';
--- synthesis translate_on 
 			TM_EN <= (others=>'0');
 			RESET_PORT <= (others=>'1');
 			CPUO <= (others=> (others=> '0'));
@@ -209,11 +219,15 @@ begin
 			T0DIV <= (others=>'1');
 			T1DIV <= (others=>'1');
 			T2DIV <= (others=>'1');
+			AUX <= (others=> (others=> '0'));
 
-			TM_CNT <= (others=>'0');
+			TM01_CNT <= (others=>'0');
+			TM2_CNT <= (others=>'0');
 			T0_CNT <= (others=>'0');
 			T1_CNT <= (others=>'0');
 			T2_CNT <= (others=>'0');
+			
+			TIMER_CE <= '0';
 		elsif rising_edge(CLK) then
 			if ENABLE = '0' then
 				if DBG_SMP_DAT_WR = '1' then
@@ -231,40 +245,20 @@ begin
 						when others => null;
 					end case;
 				end if;
-			elsif EN = '1' then
+			elsif SPC700_CE = '1' then
+				TIMER_CE <= '1';
+				
 				RESET_PORT <= "00";
-				TM_CNT <= TM_CNT + 1;
-				if TM_CNT = 127 then
-					if TM_EN(0) = '1' then
-						T0_CNT <= T0_CNT + 1;
-						if T0_CNT + 1 = unsigned(T0DIV) then
-							T0_CNT <= (others=>'0');
-							T0OUT <= std_logic_vector(unsigned(T0OUT) + 1);
-						end if;
-					end if;
-					if TM_EN(1) = '1' then
-						T1_CNT <= T1_CNT + 1;
-						if T1_CNT + 1 = unsigned(T1DIV) then
-							T1_CNT <= (others=>'0');
-							T1OUT <= std_logic_vector(unsigned(T1OUT) + 1);
-						end if;
-					end if;
-				end if;
-				if TM_CNT(3 downto 0) = 15 then
-					if TM_EN(2) = '1' then
-						T2_CNT <= T2_CNT + 1;
-						if T2_CNT + 1 = unsigned(T2DIV) then
-							T2_CNT <= (others=>'0');
-							T2OUT <= std_logic_vector(unsigned(T2OUT) + 1);
-						end if;
-					end if;
-				end if;
 				
 				if SPC700_A(15 downto 4) = x"00F" then
-					if SPC700_WE = '0' then
+					if SPC700_R_WN = '0' then
 						case SPC700_A(3 downto 0) is
 							when x"0" =>
-								TEST <= SPC700_D_OUT;
+								CLK_SPEED <= SPC700_D_OUT(7 downto 6);
+								TM_SPEED <= SPC700_D_OUT(5 downto 4);
+								TIMERS_ENABLE <= SPC700_D_OUT(3);
+								RAM_WRITE_EN <= SPC700_D_OUT(1);
+								TIMERS_DISABLE <= SPC700_D_OUT(0);
 							when x"1" =>
 								IPL_EN <= SPC700_D_OUT(7);
 								RESET_PORT <= SPC700_D_OUT(5 downto 4);
@@ -283,6 +277,8 @@ begin
 								end if;
 							when x"4" | x"5" | x"6" | x"7" =>
 								CPUO(to_integer(unsigned(SPC700_A(1 downto 0)))) <= SPC700_D_OUT;
+							when x"8" | x"9" =>
+								AUX(to_integer(unsigned(SPC700_A(0 downto 0)))) <= SPC700_D_OUT;
 							when x"A" => 
 								T0DIV <= SPC700_D_OUT;
 							when x"B" => 
@@ -294,26 +290,58 @@ begin
 					else
 						case SPC700_A(3 downto 0) is
 							when x"D" => 
-								if T0_CNT + 1 /= unsigned(T0DIV) then
-									T0OUT <= (others=>'0');
-								end if;
+								T0OUT <= (others=>'0');
 							when x"E" => 
-								if T1_CNT + 1 /= unsigned(T1DIV) then
-									T1OUT <= (others=>'0');
-								end if;
+								T1OUT <= (others=>'0');
 							when x"F" => 
-								if T2_CNT + 1 /= unsigned(T2DIV) then
-									T2OUT <= (others=>'0');
-								end if;
+								T2OUT <= (others=>'0');
 							when others => null;
 						end case;
 					end if;
 				end if;
 			end if;
+			
+			if TIMER_CE = '1' then
+				TM_STEP := ("00001" sll to_integer(unsigned(CLK_SPEED))) + ("00010" sll to_integer(unsigned(TM_SPEED)));
+				NEW_TM01_CNT := TM01_CNT + TM_STEP;
+				if NEW_TM01_CNT(8 downto 7) = "11" then
+					TM01_CNT <= NEW_TM01_CNT and "001111111";
+					if TM_EN(0) = '1' and TIMERS_ENABLE = '1' and TIMERS_DISABLE = '0' then
+						T0_CNT <= T0_CNT + 1;
+						if T0_CNT + 1 = unsigned(T0DIV) then
+							T0_CNT <= (others=>'0');
+							T0OUT <= std_logic_vector(unsigned(T0OUT) + 1);
+						end if;
+					end if;
+					if TM_EN(1) = '1' and TIMERS_ENABLE = '1' and TIMERS_DISABLE = '0' then
+						T1_CNT <= T1_CNT + 1;
+						if T1_CNT + 1 = unsigned(T1DIV) then
+							T1_CNT <= (others=>'0');
+							T1OUT <= std_logic_vector(unsigned(T1OUT) + 1);
+						end if;
+					end if;
+				else
+					TM01_CNT <= NEW_TM01_CNT;
+				end if;
+				
+				NEW_TM2_CNT := TM2_CNT + TM_STEP;
+				if NEW_TM2_CNT(5 downto 4) = "11" then
+					TM2_CNT <= NEW_TM2_CNT and "001111";
+					if TM_EN(2) = '1' and TIMERS_ENABLE = '1' and TIMERS_DISABLE = '0' then
+						T2_CNT <= T2_CNT + 1;
+						if T2_CNT + 1 = unsigned(T2DIV) then
+							T2_CNT <= (others=>'0');
+							T2OUT <= std_logic_vector(unsigned(T2OUT) + 1);
+						end if;
+					end if;
+				else
+					TM2_CNT <= NEW_TM2_CNT;
+				end if;
+			end if;
 		end if;
 	end process;
 	
-	process(SPC700_A, SPC700_WE, CPUI, DI, IPL_EN, T0OUT, T1OUT, T2OUT)
+	process(SPC700_A, CPUI, DI, IPL_EN, AUX, T0OUT, T1OUT, T2OUT)
 	begin
 		if SPC700_A(15 downto 4) = x"00F" then
 			case SPC700_A(3 downto 0) is
@@ -321,6 +349,8 @@ begin
 					SPC700_D_IN <= DI;
 				when x"4" | x"5" | x"6" | x"7" =>
 					SPC700_D_IN <= CPUI(to_integer(unsigned(SPC700_A(1 downto 0))));
+				when x"8" | x"9" =>
+					SPC700_D_IN <= AUX(to_integer(unsigned(SPC700_A(0 downto 0))));
 				when x"D" => 
 					SPC700_D_IN <= x"0" & T0OUT;
 				when x"E" => 
@@ -338,10 +368,11 @@ begin
 	end process;
 	
 	A <= SPC700_A;
-	WE <= SPC700_WE;
+	WE <= SPC700_R_WN or not RAM_WRITE_EN;
 	DO <= SPC700_D_OUT;
 
-process( DBG_REG, CPUI, CPUO, TEST, IPL_EN, RESET_PORT, TM_EN, T0DIV, T1DIV, T2DIV, T0OUT, T1OUT, T2OUT )
+process( DBG_REG, CPUI, CPUO, CLK_SPEED, TM_SPEED, TIMERS_ENABLE, TIMERS_DISABLE, RAM_WRITE_EN, 
+			IPL_EN, RESET_PORT, TM_EN, T0DIV, T1DIV, T2DIV, T0OUT, T1OUT, T2OUT, AUX )
 begin
 	case DBG_REG is
 		when x"00" => DBG_SMP_DAT <= CPUI(0);
@@ -352,7 +383,7 @@ begin
 		when x"05" => DBG_SMP_DAT <= CPUO(1);
 		when x"06" => DBG_SMP_DAT <= CPUO(2);
 		when x"07" => DBG_SMP_DAT <= CPUO(3);
-		when x"08" => DBG_SMP_DAT <= TEST;
+		when x"08" => DBG_SMP_DAT <= CLK_SPEED & TM_SPEED & TIMERS_DISABLE & "0" & RAM_WRITE_EN & TIMERS_ENABLE;
 		when x"09" => DBG_SMP_DAT <= IPL_EN & "0" & RESET_PORT & "0" & TM_EN;
 		when x"0A" => DBG_SMP_DAT <= T0DIV;
 		when x"0B" => DBG_SMP_DAT <= T1DIV;
@@ -360,6 +391,8 @@ begin
 		when x"0D" => DBG_SMP_DAT <= "0000" & T0OUT;
 		when x"0E" => DBG_SMP_DAT <= "0000" & T1OUT;
 		when x"0F" => DBG_SMP_DAT <= "0000" & T2OUT;
+		when x"10" => DBG_SMP_DAT <= AUX(0);
+		when x"11" => DBG_SMP_DAT <= AUX(1);
 		when others => DBG_SMP_DAT <= x"00";
 	end case; 
 end process;

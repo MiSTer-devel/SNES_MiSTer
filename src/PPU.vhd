1087886f9e4aea2959a4e8d17b21cfa576ee378c
sorgelig <pour.garbage@gmail.com>
2018-12-23 02:58:09 +0800

    PPU: tweaks.

--- src/PPU.vhd
+++ src/PPU.vhd
@@ -861,11 +861,11 @@ begin
 				IN_VBL <= '0';
 			end if;
 			
-			if H_CNT = 19-1  then HDE <= '1'; end if;
-			if H_CNT = 275-1 then HDE <= '0'; end if;
+			if H_CNT = 20-1  then HDE <= '1'; end if;
+			if H_CNT = 276-1 then HDE <= '0'; end if;
 
 			if H_CNT = 296-1 then HSYNC <= '1'; end if;
-			if H_CNT = 306-1 then HSYNC <= '0'; end if;
+			if H_CNT = 320-1 then HSYNC <= '0'; end if;
 
 			if V_CNT = 1               then VDE <= '1'; end if;
 			if V_CNT = LAST_VIS_LINE+1 then VDE <= '0'; end if;

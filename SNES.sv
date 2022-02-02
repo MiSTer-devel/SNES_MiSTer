3f34d64433481c20f55830475a8035dd6b52cc3b
Sorgelig <pour.garbage@gmail.com>
2022-02-02 22:56:06 +0800

    Update sys.

--- SNES.sv
+++ SNES.sv
@@ -28,7 +28,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [47:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,

6bf1e36ece5469d575abf887b65e1f55cffd81da
Sorgelig <pour.garbage@gmail.com>
2024-12-28 14:42:41 +0800

    Update sys.

--- SNES.sv
+++ SNES.sv
@@ -56,6 +56,7 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -185,6 +186,7 @@ assign BUTTONS   = osd_btn;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
 
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 

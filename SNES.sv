40c12cc20bad8d60ccb5ac218d042aa5b9adf8d7
littlegamer87 <17988200+littlegamer87@users.noreply.github.com>
2026-02-23 18:21:03 +0100

    Fix save states when rom address mirror is active

--- SNES.sv
+++ SNES.sv
@@ -871,7 +871,6 @@ reg         rom_mirror_en;
 
 always @(posedge clk_sys) begin
 	if (~old_downloading & cart_download) begin
-		rom_file_sz   <= 24'd0;
 		rom_mirror_en <= 1'b0;
 	end
 
@@ -906,7 +905,8 @@ always @(posedge clk_sys) begin
 	end
 end
 
-wire        rom_in_mirror = rom_mirror_en & |(ROM_ADDR & rom_mirror_split);
+wire        rom_in_range  = !(|(ROM_ADDR & ~rom_mask));
+wire        rom_in_mirror = rom_mirror_en & rom_in_range & |(ROM_ADDR & rom_mirror_split);
 wire [23:0] rom_addr_out  = rom_in_mirror
              ? (rom_mirror_split | (ROM_ADDR & rom_mirror_rmask))
              : ROM_ADDR;
